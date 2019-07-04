ea89bb59a1d210dd7a53f7f94739165abd7bbfca
Jason Howie <howiejason93@gmail.com>
2019-07-04 09:35:41 -0400

    focus on first text field and hide other-title

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Will do the things needed when page is loaded and ready
+$(document).ready(function() {
+	// Focuses on the first text field on page load
+	$('#name').focus();
+	// Hides the `Other title` text field on page load
+	$('#other-title').hide();
+})
