f1acaeb96524e7d9068bb4042b7a688a890c3f57
Jason Howie <howiejason93@gmail.com>
2019-07-04 10:32:54 -0400

    select tags change with user input

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@ $(document).ready(function() {
 	// Hides the `Other title` text field on page load
 	$('#other-title').hide();
 })
+
+/*
+ * Takes the design select menu and changes the color menu
+ * based on which is chosen and displays only the
+ * colors that can be selected
+ */
+function tShirtInfo() {
+	const design = $('#design');
+	const color = $('#color');
+	const colorChildren = color.children();
+	design.change(function() {
+		const designVal = design.val()
+		if (designVal === 'js puns') {
+			colorChildren.show();
+			for (let i = 3; i < colorChildren.length; i++) {
+				color[0].selectedIndex = 0;
+				colorChildren.eq(i).hide();
+			}
+		} else if (designVal === 'heart js') {
+			colorChildren.show();
+			for (let i = 0; i < 3; i++) {
+				color[0].selectedIndex = 3;
+				colorChildren.eq(i).hide();
+			}
+		} else {
+			colorChildren.show();
+			color[0].selectedIndex = 0;
+		}
+	});
+}
+
+tShirtInfo();
